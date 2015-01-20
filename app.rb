f70cef6722c7cd427814c374d61fa650b34d916c
Ellen Krause Lehman <student@epicodus.com>
2015-01-20 10:30:52 -0800

    updated for lesson

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ end
 
 post("/lists") do
   name = params.fetch("name")
-  list = List.new(:name => name, :id => nil)
+  list = List.new({:name => name, :id => nil})
   list.save()
   @lists = List.all()
   erb(:index)
@@ -29,8 +29,27 @@ end
 post("/tasks") do
   description = params.fetch("description")
   list_id = params.fetch("list_id").to_i()
-  task = Task.new(:description => description, :list_id => list_id)
+  task = Task.new({:description => description, :list_id => list_id})
   task.save()
   @list = List.find(list_id)
   erb(:list)
+end
+
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  erb(:list)
+end
+
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.delete()
+  @lists = List.all()
+  erb(:index)
 end
