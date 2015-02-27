0eef64a2d8056b96c5e25512a4c194a8c21a4f6f
Ellen Krause Lehman <ellen.k.lehman+ellen@gmail.com>
2015-02-27 11:43:28 -0800

    add validation for models

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe List do
   it { should have_many :tasks }
+  it { should validate_presence_of :name }
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Task do
   it { should belong_to :list }
+  it { should validate_presence_of :description }
 end
